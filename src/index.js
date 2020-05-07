92e0616d9c0bdfb5b4d6347e7a7e7b94485c28ef
buscaroli <m.buscaroli@outlook.com>
2020-05-07 09:37:48 +0100

    First Commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const userRouter = require('./routers/user')
+const hbs = require('hbs')
+
+const publicPath = path.join(__dirname, '../public')
+const partialPath = path.join(__dirname, '../views')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(publicPath))
+app.use(userRouter)
+app.set('view engine', 'hbs')
+hbs.registerPartials(partialPath)
+
+
+// Starting the Server
+
+app.listen(port, () => {
+    console.log(`Server up on port ${ port }`)
+})
---
+++ src/routers/user.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const router = express()
+const bodyParser = require('body-parser');
+
+const middlewares = [
+  bodyParser.urlencoded({ extended: true }),
+];
+
+
+let myDB = [
+    {
+        company: 'Firm One',
+        town:    'Poole',
+        postcode: 'BH34 2NN',
+        address:    '2000 Romanian Rd',
+        phone:      '01303123456',
+        PDA:        'Sal',   
+    },
+    {
+        company: 'Firm Two',
+        town:    'Sounthbourne',
+        postcode: 'BH43 9ZZ',
+        address:    '72 Draconian Way',
+        phone:      '01404654321',
+        PDA:        'Wendy',   
+    },
+    {
+        town: '',
+        phone: ''
+    }
+]
+
+const author = {
+    name: 'Matt',
+    company: 'Bright Firm Ltd'
+}
+
+
+router.get('', (req, res) => {
+    res.render('index', {
+        town: myDB[0].town,
+        phone: myDB[0].phone
+    })
+})
+
+router.get('/data', (req, res) => {
+    res.send(myDB)
+})
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+        name: author.name,
+        company: author.company
+    })
+})
+
+
+router.get('/query', (req, res) => {
+    
+    res.render('query', {
+        town: req.town,
+        phone: req.phone
+    }) 
+})
+
+
+router.post('/query', (req, res) => {
+    res.render('query', {
+        town: req.town,
+        phone: req.phone
+    })
+    
+})
+
+
+router.get('*', (req, res) => {
+    res.render('404', {})
+})
+
+
+module.exports = router
